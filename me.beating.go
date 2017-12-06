76937c6e19ad80aeccbda4b03bd61bcdc267f8e5
MillerD <zhangliangwu@4snow.com>
2017-12-06 17:30:18 +0800

    cors set to http://www.beating.io

--- me.beating.go
+++ me.beating.go
@@ -41,7 +41,7 @@ func fake_Data() {
 func makeHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
 	fmt.Println("make handler")
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "http://www.beating.io")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
 		if r.Method == "OPTIONS" {
